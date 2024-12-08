69c7316cf447b4fcc9977700fe1f5795f4f396c1
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-12-08 08:37:26 +0100

    feat: pay x

--- src/create_account.rs
+++ src/create_account.rs
@@ -1,4 +1,4 @@
-use solana_program::{account_info::{next_account_info, AccountInfo}, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey};
+use solana_program::{account_info::{next_account_info, AccountInfo}, entrypoint::ProgramResult, pubkey::Pubkey};
 use solana_program::program_error::ProgramError;
 use solana_program::system_instruction;
 use solana_program::sysvar::{rent::Rent, Sysvar};
@@ -6,7 +6,6 @@ pub fn process(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
     let user_account = next_account_info(accounts_iter)?;
 
-    // Check if the account is already initialized
     if user_account.data_is_empty() {
         let rent = &Rent::from_account_info(next_account_info(accounts_iter)?)?;
         let rent_exempt = rent.is_exempt(user_account.lamports(), user_account.data_len());
@@ -15,7 +14,6 @@ pub fn process(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
             return Err(ProgramError::InsufficientFunds);
         }
 
-        // Create the account by linking to the Solana wallet
         let system_program = next_account_info(accounts_iter)?;
         let create_account_ix = system_instruction::create_account(
             &system_program.key,
@@ -25,7 +23,6 @@ pub fn process(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
             program_id,
         );
 
-        // Execute the transaction
         solana_program::program::invoke(
             &create_account_ix,
             &[
